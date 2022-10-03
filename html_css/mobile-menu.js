d851a0e0aee1f03425250582d3031872179ab177
George Deyneka <zhoradeynecka@gmail.com>
2022-10-04 01:47:13 +0300

    ls_4 Added .js logics, which fixed menu close and scroll bug, also changes have been made according to the updated layout

--- html_css/mobile-menu.js
+++ html_css/mobile-menu.js
@@ -1,1 +1,24 @@
-console.log("It's working")
+let checkbox = document.querySelector("#menu-toggle");
+let body = document.querySelector("body");
+
+window.addEventListener("resize", menuAutoClose);
+checkbox.addEventListener("change", noScroll);
+
+function noScroll() {
+  checkbox.checked
+    ? (body.className = "no-scroll")
+    : body.classList.remove("no-scroll");
+}
+
+function menuAutoClose() {
+  if (!checkbox.checked) {
+    return;
+  }
+  let newWidth = document.documentElement.clientWidth;
+  this.setTimeout(() => {
+    if (newWidth >= 768) {
+      checkbox.checked = false;
+      noScroll();
+    }
+  }, 500);
+}
