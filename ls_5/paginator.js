4ecc686415701c6415d6ba6bbc324d4cc09078fb
George Deyneka <zhoradeynecka@gmail.com>
2022-10-09 04:42:51 +0300

    ls_5 Optimized code, fixed bugs

--- ls_5/paginator.js
+++ ls_5/paginator.js
@@ -1,42 +1,62 @@
 import { state } from "./state.js";
 
 const blogCards = document.querySelector(".blog__cards");
-const blogPointBtn = document.querySelectorAll(".blog__point-btn");
 const blogPoints = document.querySelector(".blog__points");
-
-blogPointBtn.forEach((el) => {
-  el.addEventListener("click", () => {
-    const currentPoint = document.querySelectorAll(".blog__point-btn_active");
-    currentPoint[0].classList.remove("blog__point-btn_active");
-    el.classList.add("blog__point-btn_active");
-  });
-});
-// I can't move this function outside because it use argument of cycle forEach
 
 function paginator(event) {
   let buttonActive;
   if (event.target.className === "blog__points") {
     return;
-    // I need this because without that rule i get empty array and cards is disappear.
-  } else {
-    buttonActive = event.target.innerText;
   }
+  buttonActive = event.target.innerText;
   getData(blogCards.children.length, buttonActive);
 }
 
-function getData(quantityOfCards, numberOfBtn = 1) {
+(function renderPaginationBtn() {
+  for (let i = 1; i <= Math.ceil(state.length / 2); i++) {
+    const blogPointBtn = document.createElement("button");
+    const blogPointLi = document.createElement("li");
+
+    blogPointLi.classList.add("blog__point");
+    blogPointBtn.classList.add("blog__point-btn");
+
+    blogPoints.appendChild(blogPointLi);
+    blogPointLi.appendChild(blogPointBtn);
+
+    blogPointBtn.innerText = `${i}`;
+    blogPointBtn.type = "button";
+
+    if (blogPointBtn.innerText == 1) {
+      blogPointBtn.classList.add("blog__point-btn_active");
+    }
+  }
+
+  const arrButtons = document.querySelectorAll(".blog__point-btn");
+
+  arrButtons.forEach((el) => {
+    el.addEventListener("click", changeActiveBtn);
+  });
+})();
+
+function changeActiveBtn(event) {
+  if (event.target.className === "blog__point") {
+    return;
+    // I have to write this condition because it fixed bug with press on <li> tag
+  }
+  document
+    .querySelector(".blog__point-btn_active")
+    .classList.remove("blog__point-btn_active");
+  event.target.classList.add("blog__point-btn_active");
+}
+
+function getData(cardsPerPage, numberOfBtn = 1) {
   const newArr = state.slice(
-    quantityOfCards * numberOfBtn - quantityOfCards,
-    quantityOfCards * numberOfBtn
+    cardsPerPage * numberOfBtn - cardsPerPage,
+    cardsPerPage * numberOfBtn
   );
   const arrCards = [...document.querySelectorAll(".blog__card")];
   switch (newArr.length) {
-    case 0:
-      // According to the agreed rules, I need to display cards depending on the existing data.
-      arrCards.forEach((el) => el.classList.add("blog__card_delete"));
-      break;
     case 1:
-      // The same thing. If array include only one element I have to display that one.
       if (
         arrCards[arrCards.length - 1].className ===
         "blog__card blog__card_delete"
@@ -62,6 +82,10 @@ function changeData(parentSelector, paginatedData) {
       el.thumbnailUrl;
     parentSelector.children[i].querySelector(".blog__avatar-img").src =
       el.avatarUrl;
+    parentSelector.children[i].querySelector(".blog__vertical-text").innerText =
+      el.category;
+    parentSelector.children[i].querySelector(".blog__read-link").innerText =
+      el.linkText;
   });
 }
 
