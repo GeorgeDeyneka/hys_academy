68fca40dc3c9f4c565e130e3e8586d5c4ceab3d6
George Deyneka <zhoradeynecka@gmail.com>
2022-11-03 21:32:49 +0200

    ls_9 Fixed http-request

--- src/js/request.js
+++ src/js/request.js
@@ -1,11 +1,11 @@
 export async function makeRequest(path = 1) {
   try {
     const response = await fetch(
-      `https://jsonplaceholder.typicode.com/albums/${path}/photos`
+      `https://jsonplaceholder.typicode.com/albums/${path}/photos?_start=0&_limit=10`
     );
     const result = await response.json();
 
-    return result.slice(0, 9);
+    return result;
   } catch (err) {
     console.log(err);
   }
