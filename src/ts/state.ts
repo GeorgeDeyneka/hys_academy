0b90b1f62edf854092c40736c795c733e3573781
George Deyneka <zhoradeynecka@gmail.com>
2023-03-09 21:39:14 +0200

    Change images for paginator, small fixes

--- src/ts/state.ts
+++ src/ts/state.ts
@@ -13,8 +13,7 @@ const DATA_PAGINATOR: PaginatorDataType[] = [
     category: "DESIGN",
     linkText: "Read Now",
     avatarUrl: "./assets/images/teacher-one.png",
-    imageUrl:
-      "https://img.freepik.com/free-photo/group-people-working-out-business-plan-office_1303-15873.jpg?w=900&t=st=1665092868~exp=1665093468~hmac=b60492185f528b22656dad556ffcde3e90c7adeea094cb43476af4a961fa786e",
+    imageUrl: "./assets/images/registration.jpg",
   },
   {
     id: 2,
@@ -22,8 +21,7 @@ const DATA_PAGINATOR: PaginatorDataType[] = [
     category: "DESIGN",
     linkText: "Read Now",
     avatarUrl: "./assets/images/teacher-two.png",
-    imageUrl:
-      "https://img.freepik.com/free-photo/two-businessmen-pointing-laptop-screen-while-discussing_158595-5324.jpg?w=900&t=st=1665093107~exp=1665093707~hmac=294dd6178a144ac531577b404e5d1e8c99cd56306209fb70f3faae378ce71b9a",
+    imageUrl: "./assets/images/meeting.jpg",
   },
   {
     id: 3,
@@ -31,8 +29,7 @@ const DATA_PAGINATOR: PaginatorDataType[] = [
     category: "DESIGN",
     linkText: "Read Now",
     avatarUrl: "./assets/images/teacher-three.png",
-    imageUrl:
-      "https://img.freepik.com/free-photo/close-up-male-hands-using-laptop-home_1150-790.jpg?t=st=1665091933~exp=1665092533~hmac=ef2ca3dd3c866bd0fb06ef7d4f1a34d952e2cada72eed4edd8a3327aeae8c822",
+    imageUrl: "./assets/images/courses.jpg",
   },
   {
     id: 4,
@@ -40,8 +37,7 @@ const DATA_PAGINATOR: PaginatorDataType[] = [
     category: "DESIGN",
     linkText: "Read Now",
     avatarUrl: "./assets/images/teacher-four.png",
-    imageUrl:
-      "https://img.freepik.com/free-photo/close-up-businessman-with-digital-tablet_1098-549.jpg?w=900&t=st=1665092658~exp=1665093258~hmac=2fd6d801df2befadb77c333436e2ca319bae5123d3a969bc2dc01fab6f8a0ea2",
+    imageUrl: "./assets/images/project.jpg",
   },
   {
     id: 5,
@@ -49,8 +45,7 @@ const DATA_PAGINATOR: PaginatorDataType[] = [
     category: "DESIGN",
     linkText: "Read Now",
     avatarUrl: "./assets/images/teacher-five.png",
-    imageUrl:
-      "https://img.freepik.com/free-photo/close-up-two-business-people-shaking-hands-while-sitting-working-place_1150-1816.jpg?w=900&t=st=1665092777~exp=1665093377~hmac=6d6a8eb7c1a8eb4fd40cae9ae2a2a951088c34d8d9d50a9fcbdf1f6c1f034433",
+    imageUrl: "./assets/images/interview.jpg",
   },
 ];
 
